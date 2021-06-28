a6db56119903aa472052ff8c4dc514a4de05ae76
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2021-06-27 21:50:38 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ document.getElementById("input").onsubmit = function() {
     var inputUrl = document.getElementById("url").value;
     inputUrl = inputUrl.split("nexturl=")[1];
     var array = inputUrl.split("otn", 1);
-    inputUrl = inputUrl[0] + "otn-pub" + inputUrl[1];
+    inputUrl = array[0] + "otn-pub" + array[1];
     var fileName = inputUrl.split("/")[8];
     var element = document.createElement('a');
     element.href = inputUrl;
